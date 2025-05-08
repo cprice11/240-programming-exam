084754a5674fad9db385c1e2d9e9c6ff764c0f63
christian <krister.price+github@gmail.com>
2025-05-08 13:47:07 -0600

    Clean up notes

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -4,9 +4,6 @@ import java.util.Objects;
 
 /**
  * Represents a single square position on a chess board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
  */
 public class ChessPosition {
     private final int rank;
