a7e3d40e8996db196cd8601e3e5dbc2cd758f32e
christian <krister.price+github@gmail.com>
2025-05-08 13:37:06 -0600

    ChessPosition constructor, getters, hash, equals, and toString

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -7,8 +9,42 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private final int rank;
+    private final int file;
 
-    public ChessPosition(int row, int col) {
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return getRank() == that.getRank() && getFile() == that.getFile();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRank(), getFile());
+    }
+
+    public ChessPosition(int rank, int file) {
+        this.rank = rank;
+        this.file = file;
+    }
+
+    /**
+     * @return which rank this position is in
+     * 1 codes for the white back rank
+     */
+    public int getRank() {
+        return rank;
+    }
+
+    /**
+     * @return which file this position is in
+     * 1 codes for the 'a' file
+     */
+    public int getFile() {
+        return file;
     }
 
     /**
@@ -16,7 +52,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return getRank();
     }
 
     /**
@@ -24,6 +60,25 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return getFile();
+    }
+
+    public char getFileChar() {
+        return switch (file) {
+            case 1 -> 'a';
+            case 2 -> 'b';
+            case 3 -> 'c';
+            case 4 -> 'd';
+            case 5 -> 'e';
+            case 6 -> 'f';
+            case 7 -> 'g';
+            case 8 -> 'h';
+            default -> '?';
+        };
+    }
+
+    @Override
+    public String toString() {
+        return getFileChar() + String.valueOf(rank);
     }
 }
